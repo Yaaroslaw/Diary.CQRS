da25f8e0adb8d9258c9e6a45cb15fca855016b57
yaaroslaw <yaaroslaw@gmail.com>
2017-05-04 15:11:15 +0300

    Add project files.

---
+++ Diary.CQRS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Diary.CQRS.Startup))]
+namespace Diary.CQRS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
